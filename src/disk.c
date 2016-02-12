c4b45f8e705f3a80d84331ac8aca9770e82cb3cd
Ben Banerjee Richards <dev@benbr.com>
2016-02-12 19:15:01 +0000

    Improved Disk struct

--- src/disk.c
+++ src/disk.c
@@ -6,21 +6,21 @@
 #include "memory.h"
 
 int disk_mount(Disk* disk) {
-	return mem_alloc(disk, DISK_SIZE);
+	return mem_alloc(&disk->data, DISK_SIZE);
 }
 
 int disk_unmount(Disk disk) {
-	return mem_free(disk);
+	return mem_free(disk.data);
 }
 
 int disk_write(Disk* disk, int location, uint8_t data) {
-	return mem_write(disk, location, data);
+	return mem_write(&disk->data, location, data);
 }
 
 uint8_t disk_read(Disk disk, int location, int* error) {
-	return mem_read(disk, location, error);
+	return mem_read(disk.data, location, error);
 }
 
 int disk_write_section(Disk* disk, int location, HeapData data) {
-	return mem_write_section(disk, location, data);
+	return mem_write_section(&disk->data, location, data);
 }
