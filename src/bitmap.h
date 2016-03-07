bca4584c10daa4ba78b0ddba6b935f0e97487877
Ben Banerjee Richards <dev@benbr.com>
2016-03-07 19:23:50 +0000

    Minor alternations

--- src/bitmap.h
+++ src/bitmap.h
@@ -7,8 +7,8 @@
 
 typedef HeapData Bitmap; 
 
-int bitmap_write(Bitmap*, int, int);
-int bitmap_read(Bitmap, int, int*);
+int bitmap_write(Bitmap* bitmap, int bit_address, int value);
+int bitmap_read(Bitmap bitmap, int bit_address, int* value);
 int bitmap_find_continuous_block_run(Bitmap, int, int, int*);
 int bitmap_find_block(Bitmap, int, int*);
 int bitmap_find_continuous_run_length(Bitmap, int, int*);
