7b726177889fa80834c205064f48ee9649513d6a
Ben Banerjee Richards <dev@benbr.com>
2016-03-10 17:49:57 +0000

    Writing to data block start

--- src/fs.h
+++ src/fs.h
@@ -146,5 +146,5 @@ int fs_create_superblock(Superblock*, uint64_t);
 int fs_add_directory_entry(Directory*, DirectoryEntry);
 int fs_directory_get_inode_number(Directory, HeapData, uint32_t*);
 int fs_allocate_blocks(Disk*, int, LList**); 
-int fs_write_data_to_disk(Disk* disk, HeapData data, LList addresses);
+int fs_write_data_to_disk(Disk* disk, HeapData data, LList addresses, int data_block);
 #endif
