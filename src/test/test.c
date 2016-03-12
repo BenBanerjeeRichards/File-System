c45ada315db624cfe894f374e34c1b39ae27a82e
Ben Banerjee Richards <dev@benbr.com>
2016-03-12 19:37:33 +0000

    Test addresses ending mid-indirect block

--- src/test/test.c
+++ src/test/test.c
@@ -232,16 +232,37 @@ static char* test_file_disk_addressing_2() {
 	stream_write_addresses(&disk, &inode, *addresses);
 	LList all_addresses = stream_read_addresses(disk, inode, &ret);
 
-	util_print_block_seq_list(all_addresses);
-
 	bool res = llist_is_equal(*addresses, all_addresses, &compare_block_sequence);
 	mu_assert("[MinUnit][TEST] file disk addressing 2: not equal pre/post serialization", res);
 
 	mem_free(disk.data_bitmap);
 	fclose(disk.file);
 	return 0;
-
 }
+
+static char* test_file_disk_addressing_3() {
+	Inode inode = {0};
+	// Partway though the first indirect block
+	const int allocation_size = 6 + 46;
+	int ret = 0;
+	Disk disk = create_fragmented_disk();
+	disk.file = fopen("fragmented.bin", "r+");
+	disk.superblock.num_used_blocks = disk.superblock.num_data_blocks - 5;
+
+	LList* addresses;
+	fs_allocate_blocks(&disk, allocation_size, &addresses);
+
+	stream_write_addresses(&disk, &inode, *addresses);
+	LList all_addresses = stream_read_addresses(disk, inode, &ret);
+
+	bool res = llist_is_equal(*addresses, all_addresses, &compare_block_sequence);
+	mu_assert("[MinUnit][TEST] file disk addressing 3: not equal pre/post serialization", res);
+
+	mem_free(disk.data_bitmap);
+	fclose(disk.file);
+	return 0;
+}
+
 
 static char* test_disk_io() {
 	Disk disk = { 0 };
@@ -939,6 +960,7 @@ static char* all_tests() {
 	mu_run_test(test_div_round_up);
 	mu_run_test(test_read_from_disk_by_seq);
 	mu_run_test(test_file_disk_addressing_2);
+	mu_run_test(test_file_disk_addressing_3);
 
 	//mu_run_test(test_inode_serialization);
 	//mu_run_test(test_alloc_blocks_non_continuous); TODO write better test
