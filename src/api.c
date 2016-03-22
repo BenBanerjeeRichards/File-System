f35c3be161ea6861eb959119e242e17e68f0b33a
Ben Banerjee-Richards <dev@benbr.com>
2016-03-22 21:22:46 +0000

    Create new file

--- src/api.c
+++ src/api.c
@@ -27,6 +27,33 @@ int api_remove_filesystem(Disk disk) {
 	return disk_remove(disk.filename);
 }
 
+int api_create_file(Disk disk, Permissions permissons, HeapData path) {
+	Inode inode = {0};
+	inode.uid = permissons.uid;
+	inode.gid = permissons.gid;
+	inode.magic = INODE_MAGIC;
+
+	int inode_num = 0;
+	int ret = fs_write_inode(disk, &inode, &inode_num);
+	if(ret != SUCCESS) return ret;
+
+	DirectoryEntry entry = {0};
+	HeapData name = {0};
+	ret = dir_get_path_name(path, &name);
+	if(ret != SUCCESS) return ret;
+
+	entry.name = name;
+	entry.inode_number = inode_num;
+
+	const int path_size = path.size;
+	path.size -= (name.size + 1);
+
+	ret = dir_add_to_directory(disk, path, entry);
+	if(ret != SUCCESS) return ret;
+
+	path.size = path_size;
+	return SUCCESS;
+}
 int api_read_all_from_file(Disk disk, int inode_number, HeapData* read_data) {
 	int ret = 0;
 
