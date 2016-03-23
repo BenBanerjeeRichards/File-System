f3b13386fef44db32cf15e4aae59afe4360f0f9c
Ben Banerjee-Richards <dev@benbr.com>
2016-03-23 21:29:03 +0000

    ^

--- src/api.h
+++ src/api.h
@@ -15,4 +15,5 @@ int api_create_file(Disk disk, Permissions permissons, HeapData path);
 int api_write_to_file(Disk disk, HeapData path, HeapData data);
 int api_read_from_file(Disk disk, int inode_number, uint64_t start_read_byte, uint64_t read_length_bytes, HeapData* read_data);
 int api_read_all_from_file(Disk disk, int inode_number, HeapData* read_data);
+int api_create_dir(Disk* disk, HeapData path, HeapData directory_name);
 #endif
